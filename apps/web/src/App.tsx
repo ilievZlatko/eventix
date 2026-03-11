de981024a00b115707f9424709f1da41b3c5f1fd
Zlatko <iliev.zlatko@gmail.com>
2026-03-11 13:43:30 +0200

    Convert apps/web from gitlink to normal folder

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function App() {
+  return (
+    <div className='flex flex-col gap-4 items-center justify-center h-screen'>
+      <p className='text-gray-600 text-2xl'>Hello from Eventix!</p>
+      <Button>Click me</Button>
+    </div>
+  )
+}
